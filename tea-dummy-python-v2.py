1f3c5d19bf249622c87fcf0eff436d9f0ebe0af9
Stella Hao <chenxuh@umich.edu>
2023-01-08 20:51:54 +0100

    small changes

---
+++ tea-dummy-python-v2.py
@@ -0,0 +1,8 @@
+# get current directory
+import os, sys
+print('current working directory: {0}'.format(os.getcwd()))
+
+# add the local python act-r folder to system path
+sys.path.insert(0, '/Users/StellaHao/Documents/ACT-R/tutorial/python')
+
+import demo2
--- tea-dummy.py
+++ tea-dummy.py
@@ -1,2 +1,10 @@
+"""
+This is a dummy model of an act-r agent making tea
+"""
+
 import pyactr as actr
 
+tea_making = actr.ACTRModel()
+
+# create chunk types
+actr.chunktype("container")

76c94d32e88031a3081f7bdef46f2672f91c6bbb
chenxuh2 <chenxuh@umich.edu>
2023-01-09 08:49:36 +0100

    Testing code
    No substantial changes; note that one must run ACT-R to use the python and ACT-R interface.

--- tea-dummy-python-v2.py
+++ tea-dummy-python-v2.py
@@ -3,6 +3,7 @@ import os, sys
 print('current working directory: {0}'.format(os.getcwd()))
 
 # add the local python act-r folder to system path
-sys.path.insert(0, '/Users/StellaHao/Documents/ACT-R/tutorial/python')
+sys.path.insert(0, 'C:/Users/we17lapo/Documents/ACT-R/tutorial/python')
 
+# Note that to connect to ACT-R through the python interface, one must run ACT-R first
 import demo2
